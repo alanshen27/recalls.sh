ee82da9ffbdb181b0487702e8e84f8d070a3d2df
Alan Shen <alan.shen27@gmail.com>
2025-05-28 18:01:14 +0200

    fix: just work pls

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
